43401b0667a4c98d6f42bfafbf4734923e198db4
Toutchy kipik <toutchy.kipik@pm.me.Toutchy kipik>
2022-11-29 17:23:47 +0100

    test

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,13 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
-    'static'
+    'static/'
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

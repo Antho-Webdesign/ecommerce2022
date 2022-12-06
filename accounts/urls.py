07d15dc7fd3e763f4fc8878599db497346fd9401
Toutchy kipik <toutchy.kipik@pm.me.Toutchy kipik>
2022-12-06 20:12:36 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 
-from accounts.views import signup, login_user, logout_user, password_reset_form, password_reset_form_done, password_reset_confirm, password_reset_complete
+from accounts.views import signup, login_user, logout_user, password_reset_form, \
+                            password_reset_form_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
+    # Inscription
     path('signup/', signup, name='signup'),
+    # Connexion
     path('login/', login_user, name='login'),
+    # Déconnexion
     path('logout/', logout_user, name='logout'),
+    # Reset password form
     path('password_reset/form/', password_reset_form, name='password_reset_form'),
+    # Reset password done
     path('password_reset/done/', password_reset_form_done, name='password_reset_form_done'),
+    # Reset password confirm
     path('password_reset/confirm/<str:token>', password_reset_confirm, name='password_reset_confirm'),
+    # Reset password complete
     path('password_reset/complete/', password_reset_complete, name='password_reset_complete'),
 ]
